fe491f7cc60deb1602849533d2e6a1bbd3aa86fa
Mathieu Lecarme <mathieu@garambrogne.net>
2011-05-06 21:19:04 +0200

    skeleton

---
+++ src/clamd_clamscan.erl
@@ -0,0 +1,91 @@
+-module(clamd_clamscan).
+
+-behaviour(gen_server).
+
+%% gen_server callbacks
+-export([start_link/0, init/1, handle_call/3, handle_cast/2, 
+handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {socket}).
+
+%%====================================================================
+%% api callbacks
+%%====================================================================
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [{127,0,0,1}, 514], []).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([Host, Port]) ->
+    case gen_connect:open(Host, Port, [binary, {packet, 0}]) of
+        {ok, Socket} -> 
+            {ok, #state{
+                socket = Socket
+            }};
+        {error, Reason} -> {stop, Reason}
+    end.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call({scan, _Path}, _From, State) ->
+    {reply, ok, State};
+handle_call(_Msg, _From, State) ->
+    {reply, ok, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+%%handle_cast({send, Who, Level, Msg}, State) ->
+    %%    handle_cast({send, 0, Who, Level, Msg}, State);
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
