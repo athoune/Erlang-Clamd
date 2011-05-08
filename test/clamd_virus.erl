68d9feeda5892a2f8fa2991ea5b268e30f6a3856
Mathieu Lecarme <mathieu@garambrogne.net>
2011-05-08 21:42:14 +0200

    simple eunit test

---
+++ test/clamd_virus.erl
@@ -0,0 +1,14 @@
+-module(clamd_virus).
+
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+
+ping_test() ->
+    application:start(clamd),
+    {ok, _} = clamd:ping().
+% stream_test() ->
+%     application:start(clamd),
+%     clamd:stream("X5O!P%@AP[4\PZX54(P^)7CC)7}"),
+%     clamd:stream("$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"),
+%     clamd:stream("").
